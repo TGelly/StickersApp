65e33198fc4ba81f2e761bd4a7fc9a1fd9db913e
TGelly <tgelly@protonmail.com>
2022-05-09 07:55:52 +0000

    fix

--- src/com/uca/dao/_Initializer.java
+++ src/com/uca/dao/_Initializer.java
@@ -14,6 +14,7 @@ public class _Initializer {
             statement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS users (userID int primary key auto_increment, role varchar(10), firstname varchar(100), lastname varchar(100), username varchar(100), password varchar(100)); ");
             statement.executeUpdate();
 
+            /*
             statement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS class (classID int primary key auto_increment, containerTeacher int, foreign key (containerTeacher) references users(userID)); ");
             statement.executeUpdate();
 
@@ -22,6 +23,7 @@ public class _Initializer {
 
             statement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS sticker (stickerID int primary key auto_increment, stickerDate varchar(100), color varchar(8), comment varchar(250), containerBook int, teacher int, foreign key (containerBook) references book(bookID), foreign key (teacher) references user(userID)); ");
             statement.executeUpdate();
+            */
 
             //Todo Remove me !
             statement = connection.prepareStatement("INSERT INTO users(firstname, lastname) VALUES(?, ?);");
