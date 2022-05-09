c54044a6a92652b86f67ea3abf3ae68cd30a8112
TGelly <tgelly@protonmail.com>
2022-05-09 07:51:58 +0000

    database creation

--- src/com/uca/dao/_Initializer.java
+++ src/com/uca/dao/_Initializer.java
@@ -11,10 +11,17 @@ public class _Initializer {
             PreparedStatement statement;
 
             //Init articles table
-            statement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS users (id int primary key auto_increment, firstname varchar(100), lastname varchar(100)); ");
+            statement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS users (userID int primary key auto_increment, role varchar(10), firstname varchar(100), lastname varchar(100), username varchar(100), password varchar(100)); ");
             statement.executeUpdate();
 
-            statement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS books (id int primary key auto_increment, firstname")
+            statement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS class (classID int primary key auto_increment, containerTeacher int, foreign key (containerTeacher) references users(userID)); ");
+            statement.executeUpdate();
+
+            statement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS books (bookID int primary key auto_increment, childName varchar(100), containerClass int, containerParent int), foreign key (containerClass) references class(classID), foreign key (containerParent) references user(userID)); ");
+            statement.executeUpdate();
+
+            statement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS sticker (stickerID int primary key auto_increment, stickerDate varchar(100), color varchar(8), comment varchar(250), containerBook int, teacher int, foreign key (containerBook) references book(bookID), foreign key (teacher) references user(userID)); ");
+            statement.executeUpdate();
 
             //Todo Remove me !
             statement = connection.prepareStatement("INSERT INTO users(firstname, lastname) VALUES(?, ?);");
