eb20de1edb76cfbf14ac001f3e1d42e69889e3bd
TGelly <tgelly@protonmail.com>
2022-05-10 08:15:08 +0000

    ?

--- bin/default/src/main/resources/static/SPA.js
+++ bin/default/src/main/resources/static/SPA.js
@@ -1,8 +1,4 @@
 page = document.getElementById("page")
-booksButton = document.getElementById("books")
-usersButton = document.getElementById("users")
-booksButton.onclick = books
-usersButton.onclick = users
 
 try {   xhr = new ActiveXObject('Msxml2.XMLHTTP');   }
     catch (e) 
@@ -14,24 +10,19 @@ try {   xhr = new ActiveXObject('Msxml2.XMLHTTP');   }
            catch (e3) {  xhr = false;   }
          }
     }
-xhr.responseType = "json";
 
-function books(){
-    console.log("Displaying books")
-    xhr.open("GET", "/books")
+xhr.responseType = "text"; //will be changed to "json" later
+
+function updatePage(url){
+    console.log("click on"+ url)
+    xhr.open("GET", url)
     xhr.send()
     xhr.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
-            console.log(this.response);
-            updatePage(this.response)
+            console.log("Request succeeded")
+            page.innerHTML = this.response;
         }
     }
 }
-function users(){
-    console.log("Displaying users")
-    page.innerHTML = "Users display"
-}
 
-function updatePage(content){
-    page.innerHTML = content;
-}
+updatePage("/frontpage.html")
